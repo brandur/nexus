6caa2ec5354bdf431e523ca854c531a58d2870e3
Brandur <brandur@mutelight.org>
2012-08-30 10:21:30 -0700

    Adding facts

--- Gemfile
+++ Gemfile
@@ -1,12 +1,15 @@
 source :rubygems
 
+gem "excon"
 gem "feedzirra"
 gem "htmlentities"
+gem "multi_json"
 gem "pg"
 gem "sequel", require: ["sequel", "sequel/extensions/pg_hstore"]
 gem "slides"
 gem "term-ansicolor"
 gem "twitter"
+gem "uuidtools"
 
 group :development do
   gem "debugger"
--- app.rb
+++ app.rb
@@ -13,6 +13,10 @@ end
 # Utilities
 #
 
+def outside_core_hours?
+  !(7..19).include?((Time.now.utc.hour-6) % 24)
+end
+
 def twitter_url(user, status_id)
   "https://twitter.com/#{user}/status/#{status_id}"
 end
@@ -21,22 +25,52 @@ end
 # Sources
 #
 
+source :facts do
+  # facts source is completely randomized, only spit one of these out
+  # periodocially and only during core PST hours
+  if Time.now.min % 10 == 0 && !outside_core_hours?
+    api = Excon.new("https://facts-api.herokuapp.com", ssl_verify_peer: false)
+    fact = MultiJson.decode(api.get(path: "/facts/random", expects: 200).body).first
+    [{
+      content: fact["content"],
+      tag: UUIDTools::UUID.random_create.to_s,
+      published_at: Time.now.utc,
+      url: "https://facts.brandur.org/#{fact["category"]["slug"]}/#{fact["id"]}",
+      metadata: {
+        category: fact["category"]["name"]
+      }
+    }]
+  else
+    []
+  end
+end
+
 source :github_heroku do
   feed = Feedzirra::Feed.fetch_and_parse(env!("GITHUB_HEROKU_URL"))
   feed.entries.map do |entry|
-    { title: entry.title, content: entry.content, tag: entry.id,
-      url: entry.url, published_at: entry.published }
+    {
+      title: entry.title,
+      content: entry.content,
+      tag: entry.id,
+      url: entry.url,
+      published_at: entry.published
+    }
   end
 end
 
 source :hackernews do
   feed = Feedzirra::Feed.fetch_and_parse("http://news.ycombinator.com/rss")
   feed.entries.map do |entry|
-    { title: entry.title, tag: entry.summary.gsub(/^.*id=(\d+).*$/, '\\1'), 
+    {
+      title: entry.title,
+      tag: entry.summary.gsub(/^.*id=(\d+).*$/, '\\1'), 
       # Hacker News doesn't include a published date of any kind ...
-      url: entry.url, published_at: Time.now.utc, metadata: {
+      url: entry.url,
+      published_at: Time.now.utc,
+      metadata: {
         comments_url: entry.summary.gsub(/^.*"(http:.*)".*$/, '\\1')
-    } }
+      }
+    }
   end
 end
 
@@ -46,14 +80,18 @@ source :twitter_brandur do
     content = tweet.text
     # expand urls, because short urls are terrible
     tweet.urls.each { |url| content.sub!(url.url, url.expanded_url) }
-    { content: tweet.text, tag: tweet.id.to_s,
+    {
+      content: tweet.text,
+      tag: tweet.id.to_s,
       url: twitter_url(tweet.user.screen_name, tweet.id),
-      published_at: tweet.created_at, metadata: {
+      published_at: tweet.created_at,
+      metadata: {
         in_reply_to_url: tweet.in_reply_to_status_id ?
           twitter_url(tweet.in_reply_to_screen_name, tweet.in_reply_to_status_id) : nil,
         in_reply_to_status: tweet.in_reply_to_status_id,
         in_reply_to_user: tweet.in_reply_to_screen_name,
-        user: tweet.user.screen_name, name: tweet.user.name
+        user: tweet.user.screen_name,
+        name: tweet.user.name
     } }
   end
 end
