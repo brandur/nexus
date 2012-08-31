5989d4b30c40745806eac37eccf7d281db940059
Brandur <brandur@mutelight.org>
2012-08-30 19:48:51 -0700

    Require authentication

--- web.rb
+++ web.rb
@@ -3,8 +3,31 @@
 #
 
 helpers do
+  def authenticate_with_http_basic
+    @auth ||= Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials &&
+      yield(@auth.credentials)
+  end
+
+  def authorized?
+    authenticate_with_http_basic do |username, password|
+      username == env!("HTTP_API_KEY")
+    end
+  end
+
+  def authorized!
+    unless authorized?
+      log :unauthorized
+      throw(:halt, [401, { message: "Not authorized" }.to_json])
+    end
+  end
+
   def curl?
     !!(request.user_agent =~ /curl/)
+  end
+
+  def log(action, attrs = {})
+    Slides.log(action, attrs.merge!(id: request.id))
   end
 end
 
@@ -24,6 +47,7 @@ end
 #
 
 get "/events" do
+  authorized!
   events = Event.order(:id.desc).limit(params[:count] || 100).map(&:to_json_v1)
   [200, MultiJson.encode(events, pretty: curl?)]
 end
